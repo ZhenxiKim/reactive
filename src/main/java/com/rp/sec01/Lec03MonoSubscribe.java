d33da05b316ef2cdf44ed2802078f5a75c8cdf71
zhenxikim <8055kjh@gmail.com>
2021-10-24 01:28:40 +0900

    Lec03MonoSubscribe

---
+++ src/main/java/com/rp/sec01/Lec03MonoSubscribe.java
@@ -0,0 +1,18 @@
+package com.rp.sec01;
+
+import reactor.core.publisher.Mono;
+
+public class Lec03MonoSubscribe {
+    public static void main(String[] args) {
+        //publisher
+        Mono<String> mono = Mono.just("ball");
+        mono.subscribe(
+                item -> System.out.println("item"),
+                err-> System.out.println(err.getMessage()),
+                () -> System.out.println("Completed")
+        );
+        //public final reactor.core.Disposable subscribe(@reactor.util.annotation.Nullable java.util.function.Consumer<? super T> consumer, @reactor.util.annotation.Nullable java.util.function.Consumer<? super java.lang.Throwable> errorConsumer, @reactor.util.annotation.Nullable java.lang.Runnable completeConsumer) { /* compiled code */ }
+
+
+    }
+}
