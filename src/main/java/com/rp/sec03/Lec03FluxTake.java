5cdc08d8637f34802a411f6cd0956ae3e0dc9656
zhenxikim <8055kjh@gmail.com>
2021-11-16 23:13:28 +0900

    Lec03FluxTake - issue정리

--- src/main/java/com/rp/sec03/Lec03FluxTake.java
+++ src/main/java/com/rp/sec03/Lec03FluxTake.java
@@ -7,8 +7,9 @@ public class Lec03FluxTake{
     public static void main(String[] args) {
         Flux.range(1,10)
                 .log()
-                .take(3)
+                .take(3)//cancels 구독취소 complete signal to the downstream
                 .log()
                 .subscribe(Util.subscriber());
+        //구독 취소하자마자 upstream은 4번쨰 아이템 방출하지않음
     }
 }
